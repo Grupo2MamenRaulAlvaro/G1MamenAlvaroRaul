6e7f8bcbac7ba59ca642b5c6c76dd5453302e0a6
Mamen <Mamen@LAPTOP-SIC9R6KM>
2017-04-18 13:15:18 +0200

    Subida del juego 1 con cambio de nombre ahora es PiedraPapelTijera.

---
+++ JuegoGrupo2/src/PiedraPapelTijera.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Juego1 {
+	public static void main(String[] ar) {
+		  Scanner teclado=new Scanner(System.in);
+		  String usu,cpu;
+		  String[] opc={"piedra","papel","tijeras"};
+		  
+		        System.out.println("Elige:");
+		  usu=teclado.next();
+		  int num = (int) Math.floor(Math.random()*(2-0+1)+0);   cpu = opc[num];
+		  switch (usu) {
+		   case "piedra":
+		    switch (cpu){
+		     case "piedra":
+		      System.out.println("CPU elije PIEDRA. EMPATE");
+		      break;
+		     case "papel":
+		      System.out.println("CPU elije PAPEL. Gana CPU");
+		      break;
+		     case "tijeras":
+		      System.out.println("CPU elije TIJERAS. Ganas TU");
+		      break;
+		    }
+		          break;
+		      case "papel":
+		    switch (cpu){
+		    case "piedra":
+		     System.out.println("CPU elije PIEDRA. Ganas TU");
+		     break;
+		    case "papel":
+		     System.out.println("CPU elije PAPEL. EMPATE");
+		     break;
+		    case "tijeras":
+		     System.out.println("CPU elije TIJERAS. Gana CPU");
+		     break;
+		   }
+		         break;
+		      case "tijeras":
+		    switch (cpu){
+		    case "piedra":
+		     System.out.println("CPU elije PIEDRA. Gana CPU");
+		     break;
+		    case "papel":
+		     System.out.println("CPU elije PAPEL. Ganas TU");
+		     break;
+		    case "tijeras":
+		     System.out.println("CPU elije TIJERAS. EMPATE");
+		     break;
+		   }
+		         break;
+		      default:
+		          System.out.println("Incorrecto");
+		          break;
+		  }
+		 }
+}
