83d3d1b3e7b9bc11c1c22c132601bf83005295cb
Raul <Marta@Marta>
2017-04-18 13:21:17 +0200

    Subida del juego Cara o Cruz

---
+++ JuegoGrupo2/src/CaraOCruz.java
@@ -0,0 +1,15 @@
+
+public class CaraOCruz {
+	 public static void caracruz(String[] args){
+		   
+		   int aleatorio = (int)(Math.random()*100);
+		   
+		   if (aleatorio>=50){
+			   System.out.println("Es cara");
+		   }else if (aleatorio <=49){
+			   System.out.println("Es cruz");
+		   }
+			   
+	   }
+
+}
