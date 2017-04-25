f40e3660eadbbaa1a7c9a5e664f557ce0e438ced
Mamen <Mamen@192.168.1.5>
2017-04-25 21:07:55 +0200

    He retocado el codigo del main para que vuelva a pedir otro juego

--- JuegoGrupo2/src/Main.java
+++ JuegoGrupo2/src/Main.java
@@ -2,7 +2,11 @@
 import java.util.*;
 
 
-
+/**
+ * Proyecto Juegos
+ * @author Mamen, Alvaro, Raúl
+ *
+ */
 public class Main {
 
 
@@ -11,32 +15,47 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("-------------------------------");
-		System.out.println("------ Elije un juego: ------- ");
-		System.out.println("-------------------------------");
-		System.out.println("--- 1. Piedra, papel o tijera -");
-		System.out.println("--- 2. Cara o cruz.------------");
-		System.out.println("--- 3. Acertar número ---------");
-		System.out.println("--- 0. Salir ------------------");
-		System.out.println("-------------------------------");
-		
-		int opcion = input.nextInt();
+		/**
+		 * Opciones que aparcen por consola para que el usuario elija alguna de ellas
+		 */
+		int opcion = 1;
+		while(opcion != 0){
+			
+			System.out.println("-------------------------------");
+			System.out.println("------ Elije un juego: ------- ");
+			System.out.println("-------------------------------");
+			System.out.println("--- 1. Piedra, papel o tijera -");
+			System.out.println("--- 2. Cara o cruz.------------");
+			System.out.println("--- 3. Acertar número ---------");
+			System.out.println("--- 0. Salir ------------------");
+			System.out.println("-------------------------------");
+			
+			 opcion = input.nextInt();
 
 
-		if (opcion == 1){
+			/**
+			 * Condiciones que debe 
+			 */
+			if (opcion == 1){
+				
+				
+				PiedraPapelTijera juego1 =new PiedraPapelTijera();
+				 juego1.juego1();
+				
+			}else if(opcion == 2){
+				
+				CaraOCruz juego2 =new CaraOCruz();
+				juego2.caraOCruz();
+				
+			}else if(opcion == 3){
+				AcertarNumero juego3 =new AcertarNumero();
+				juego3.juego();
+				
+			}else if(opcion == 0){
+				System.out.println("No deseo jugar más");
+			}
 			
 			
-			PiedraPapelTijera juego1 =new PiedraPapelTijera();
-			 juego1.juego1();
-			
-		}else if(opcion == 2){
-			
-			CaraOCruz juego2 =new CaraOCruz();
-			juego2.caraOCruz();
-			
-		}else if(opcion == 3){
-			AcertarNumero juego3 =new AcertarNumero();
-			juego3.juego();
 		}
 		
 		
