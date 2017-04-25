f113a2e94145ed9976748c45f8cb5dfd9aeb4e22
FuaFua <FuaFua@FuaFua-Win10>
2017-04-25 12:48:14 +0200

    Actualizacion del main y juego3

--- JuegoGrupo2/src/AcertarNumero.java
+++ JuegoGrupo2/src/AcertarNumero.java
@@ -2,25 +2,60 @@
 
 import java.util.Scanner;
 
+/**
+ * Juego para acertar un número
+ * @author FuaFua
+ *
+ */
+
 public class AcertarNumero {
-
-	public static void main(String[] args) {
-
+	
+	
+	/**
+	 * metodo
+	 */
+public void juego(){
+	
+	
+			/**
+			 * variable donde se genera el numero a acertar
+			 */
 		int aleatorio=(int)(Math.random()*100);
 
+			/**
+			 * para introducir datos por consola
+			 */
 		Scanner entrada=new Scanner(System.in);
 		
+			/**
+			 * numero que introduciremos para acertarlo 
+			 */
 		int numero=0;
 		
+			/**
+			 * Para indicar los intentos usados
+			 */
 		int intentos=0;
 		
+		/**
+		 * Bucle que si se acierta el numero se sale de este para finalizar el juego
+		 */
 		do{
+			/**
+			 * incrementamos en 1 los intentos a mostrar
+			 */
 			intentos++;
 			
+				/**
+				 * los dos siguintes son para leer el numero para jugar 
+				 */
 			System.out.println("Introduce un número: ");
 			
 			numero=entrada.nextInt();
 			
+			/**
+			 * Esto es una ayuda para indicar si el numero a acertar esta más alto o bajo del numero introducido
+			 */
 			if(aleatorio<numero){
 				
 				System.out.println("Esta mas bajo");
@@ -33,15 +68,20 @@ public class AcertarNumero {
 			
 		}while (numero!=aleatorio);
 		
+		
+		/**
+		 * Esto dice que has acertado y los intentos usados
+		 */
 		System.out.println("Correcto");
 		System.out.println("Has usado " + intentos + " intentos");
 		
 	}
 
-
+}
 
 		
 
-	}
+	
+	
 
 
--- JuegoGrupo2/src/Main.java
+++ JuegoGrupo2/src/Main.java
@@ -1,8 +1,44 @@
 
+import java.util.*;
+
+
+
 public class Main {
 
+
+	
+	static Scanner input=new Scanner (System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("-------------------------------");
+		System.out.println("------ Elije un juego: ------- ");
+		System.out.println("-------------------------------");
+		System.out.println("--- 1. Piedra, papel o tijera -");
+		System.out.println("--- 2. Cara o cruz.------------");
+		System.out.println("--- 3. Acertar número ---------");
+		System.out.println("--- 0. Salir ------------------");
+		System.out.println("-------------------------------");
+		
+		int opcion = input.nextInt();
+
+
+		if (opcion == 1){
+			
+			AcertarNumero juego1 =new AcertarNumero();
+			juego1.juego();
+			
+		}else if(opcion == 2){
+			
+			AcertarNumero juego2 =new AcertarNumero();
+			juego2.juego();
+			
+		}else if(opcion == 3){
+			AcertarNumero juego3 =new AcertarNumero();
+			juego3.juego();
+		}
+		
+		
 
 	}
 
