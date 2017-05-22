e393d73717ceddc96b286fd240911e2b30836e01
fuafua <alvaro.martin.fernandez@alumnos.fesac.es>
2017-05-22 20:27:50 +0200

    Actualizacion de comentarios de Cara o Cruz y Acertar numero

--- JuegoGrupo2/src/AcertarNumero.java
+++ JuegoGrupo2/src/AcertarNumero.java
@@ -5,57 +5,67 @@ import java.util.Scanner;
 /**
  * Juego para acertar un número
  * @author FuaFua
- *
+ * @version 1.0
  */
 
 public class AcertarNumero {
 	
 	
-	/**
-	 * metodo
-	 */
+	
 public void juego(){
+	
+	/**
+	 * Juego que pide un numero al usuario, y lo compara con otro del 1 al 1oo generado automaticamente
+	 * El jugador gana cuando acierta el numero generado
+	 * @param
+	 */
+	
 	
 	
 			/**
-			 * variable donde se genera el numero a acertar
+			 * Genera aleatoriamente un número del 1 al 100
+			 * @param
 			 */
 		int aleatorio=(int)(Math.random()*100);
 
 			/**
-			 * para introducir datos por consola
+			 * Para que pueda leer un numero
+			 * @param
 			 */
 		Scanner entrada=new Scanner(System.in);
 		
-			/**
-			 * numero que introduciremos para acertarlo 
-			 */
+			
 		int numero=0;
-		
-			/**
-			 * Para indicar los intentos usados
-			 */
+			
 		int intentos=0;
 		
 		/**
-		 * Bucle que si se acierta el numero se sale de este para finalizar el juego
+		 * Bucle que no termina hasta que se introduce el numero correcto
+		 * @param
 		 */
 		do{
 			/**
 			 * incrementamos en 1 los intentos a mostrar
+			 * @param
 			 */
 			intentos++;
 			
-				/**
-				 * los dos siguintes son para leer el numero para jugar 
-				 */
+				
 			System.out.println("Introduce un número: ");
 			
+			/**
+			 * Lee el número introducido
+			 * @param
+			 */
 			numero=entrada.nextInt();
 			
 			/**
 			 * Esto es una ayuda para indicar si el numero a acertar esta más alto o bajo del numero introducido
+			 * Si el número introducido es mas bajo que el generado, se imprime que esta mas bajo, si no, imprime
+			 * que esta más alto
+			 * @param
 			 */
+			
 			if(aleatorio<numero){
 				
 				System.out.println("Esta mas bajo");
@@ -65,12 +75,16 @@ public void juego(){
 				System.out.println("Esta mas alto");
 			}
 			
-			
+			/**
+			 * Se esta indicando que si el numero introducido es distinto al generado, que no salga del bucle
+			 * @param
+			 */
 		}while (numero!=aleatorio);
 		
 		
 		/**
-		 * Esto dice que has acertado y los intentos usados
+		 * Cuando acertamos el número, esto nos dice los intentos utilizados
+		 * 
 		 */
 		System.out.println("Correcto");
 		System.out.println("Has usado " + intentos + " intentos");
--- JuegoGrupo2/src/CaraOCruz.java
+++ JuegoGrupo2/src/CaraOCruz.java
@@ -2,19 +2,22 @@
 /**
  *
  * @author Raul
- *
+ * @version 25/04/2014
  */
 public class CaraOCruz {
 		 
 		 
 		/**
-		 * Juego que lanza la moneda
+		 * Juego que lanza la moneda.
+		 * 
 		 */
 		 	public void caraOCruz(){
 	
 		   
 		   int aleatorio = (int)(Math.random()*100);
-		   
+		 /**
+		  * @param es un bucle que te dice si la moneda es cara o cruz.
+		  */
 		   if (aleatorio>=50){
 			   System.out.println("Es cara");
 		   }else if (aleatorio <=49){
