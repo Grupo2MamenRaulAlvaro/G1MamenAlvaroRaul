5435721eaeac1b4bff92621d97dcae80ed8e425d
Mamen <Mamen@192.168.1.3>
2017-05-22 21:21:44 +0200

    Implemento javadoc en PiedraPapelTijeras

--- JuegoGrupo2/src/PiedraPapelTijera.java
+++ JuegoGrupo2/src/PiedraPapelTijera.java
@@ -7,65 +7,165 @@ import java.util.Scanner;
  *
  */
 public class PiedraPapelTijera {
+	
+	// Constantes
+	private static final String TIJERAS = "tijeras";
+	private static final String PAPEL = "papel";
+	private static final String PIEDRA = "piedra";
+	
+	// Atributos
+	private String usuario;
+	private String cpu;
+	
+	
 	/**
-	 * metodo que realiza el juego
+	 * <h1>Constructor por defecto</h1>
 	 */
-	 public void juego1(){
-		  Scanner teclado=new Scanner(System.in);
-		  String usu,cpu;
-		  String[] opc={"piedra","papel","tijeras"};
-		  
-		        System.out.println("Elige:");
-		  usu=teclado.next();
-		  /**
-		   * Genera aleatoriamente la cpu
-		   */
-		  int num = (int) Math.floor(Math.random()*(2-0+1)+0);   cpu = opc[num];
-		  switch (usu) {
-		   case "piedra":
-		    switch (cpu){
-		     case "piedra":
-		      System.out.println("CPU elije PIEDRA. EMPATE");
-		      break;
-		     case "papel":
-		      System.out.println("CPU elije PAPEL. Gana CPU");
-		      break;
-		     case "tijeras":
-		      System.out.println("CPU elije TIJERAS. Ganas TU");
-		      break;
-		    }
-		          break;
-		      case "papel":
-		    switch (cpu){
-		    case "piedra":
-		     System.out.println("CPU elije PIEDRA. Ganas TU");
-		     break;
-		    case "papel":
-		     System.out.println("CPU elije PAPEL. EMPATE");
-		     break;
-		    case "tijeras":
-		     System.out.println("CPU elije TIJERAS. Gana CPU");
-		     break;
-		   }
-		         break;
-		      case "tijeras":
-		    switch (cpu){
-		    case "piedra":
-		     System.out.println("CPU elije PIEDRA. Gana CPU");
-		     break;
-		    case "papel":
-		     System.out.println("CPU elije PAPEL. Ganas TU");
-		     break;
-		    case "tijeras":
-		     System.out.println("CPU elije TIJERAS. EMPATE");
-		     break;
-		   }
-		         break;
-		      default:
-		          System.out.println("Incorrecto");
-		          break;
-		  }
-	 }
+	 public PiedraPapelTijera() {
+		
+	}
+	 
+	 
+	/**
+	 * <h1>Constructor con 2 parametros</h1>
+	 * @param usu
+	 * @param cpu
+	 */
+	 public PiedraPapelTijera(String usuario, String cpu) {
+		super();
+		this.usuario = usuario;
+		this.cpu = cpu;
+	}
+
+
+	/**
+	 * <h1>Menu del juego</h1>
+	 * @return void 
+	 */
+	@SuppressWarnings("resource")
+	public void juego1() {
+		Scanner teclado = new Scanner(System.in);
+
+		String[] opc = { PIEDRA, PAPEL, TIJERAS };
+
+		System.out.println("Elige: ");
+		usuario = teclado.next();
+		
+		// Genera aleatoriamente la cpu
+		randomGenerateCpu(opc);
+		
+		switch (usuario) {
+		case PIEDRA:
+			cpuPiedraCase();
+			break;
+		case PAPEL:
+			cpuPapelCase();
+			break;
+		case TIJERAS:
+			cpuTijerasCase();
+			break;
+		default:
+			System.out.println("Incorrecto");
+			break;
+		}
+	}
+
+
+	/**
+	 * Case de cpu para el caso en que usuario elija Tijeras
+	 */
+	public void cpuTijerasCase() {
+		switch (cpu) {
+		case PIEDRA:
+			System.out.println("CPU elije PIEDRA. Gana CPU");
+			break;
+		case PAPEL:
+			System.out.println("CPU elije PAPEL. Ganas TU");
+			break;
+		case TIJERAS:
+			System.out.println("CPU elije TIJERAS. EMPATE");
+			break;
+		}
+	}
+
+
+	/**
+	 * Case de cpu para el caso en que usuario elija Papel
+	 */
+	public void cpuPapelCase() {
+		switch (cpu) {
+		case PIEDRA:
+			System.out.println("CPU elije PIEDRA. Ganas TU");
+			break;
+		case PAPEL:
+			System.out.println("CPU elije PAPEL. EMPATE");
+			break;
+		case TIJERAS:
+			System.out.println("CPU elije TIJERAS. Gana CPU");
+			break;
+		}
+	}
+
+
+	/**
+	 * Case de cpu para el caso en que usuario elija Piedra 
+	 */
+	public void cpuPiedraCase() {
+		switch (cpu) {
+		case PIEDRA:
+			System.out.println("CPU elije PIEDRA. EMPATE");
+			break;
+		case PAPEL:
+			System.out.println("CPU elije PAPEL. Gana CPU");
+			break;
+		case TIJERAS:
+			System.out.println("CPU elije TIJERAS. Ganas TU");
+			break;
+		}
+	}
+
+
+	/**
+	 * Genera aleatoriamente la cpu
+	 * @param opc
+	 */
+	public void randomGenerateCpu(String[] opc) {
+		int num = (int) Math.floor(Math.random() * (2 - 0 + 1) + 0);
+		cpu = opc[num];
+	}
+	
+	
+	 /**
+	  * Get del atributo usu
+	  * @return usu
+	  */
+	public String getUsu() {
+		return usuario;
+	}
+
+
+	/**
+	 * Set del atributo usu
+	 * @param usu
+	 */
+	public void setUsu(String usu) {
+		this.usuario = usu;
+	}
+
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getCpu() {
+		return cpu;
+	}
+
+
+	public void setCpu(String cpu) {
+		this.cpu = cpu;
+	}
+
 }
 		  
 		 
